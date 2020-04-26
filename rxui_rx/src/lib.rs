f8ad87ec3f5e4497bcd55e62974ad395cfc96a9b
Alexander Linne <alexander.linne@outlook.com>
2020-04-26 23:27:37 +0200

    add prelude, add into_observable for IntoIterator

--- rxui_rx/src/lib.rs
+++ rxui_rx/src/lib.rs
@@ -3,34 +3,4 @@ pub mod observable;
 pub mod observer;
 pub mod subscriber;
 
-#[cfg(test)]
-mod test {
-    use crate::core::*;
-    use crate::observable;
-    use crate::observer;
-    use std::thread;
-
-    #[test]
-    fn test() {
-        let observable = observable::create(|mut subscriber| {
-            if !subscriber.is_unsubscribed() {
-                subscriber.on_next(1);
-            }
-        });
-        observable.subscribe(observer::from_next_fn(|v| {
-            println!("{}", v);
-        }));
-    }
-
-    #[test]
-    fn test_send() {
-        let observable = observable::create(|mut subscriber| {
-            if !subscriber.is_unsubscribed() {
-                subscriber.on_next(1);
-            }
-        });
-        thread::spawn(move || observable.subscribe(observer::from_next_fn(|v| {
-            println!("{}", v);
-        })));
-    }
-}
+pub mod prelude;
--- rxui_rx/src/observable/iter.rs
+++ rxui_rx/src/observable/iter.rs
@@ -1,1 +1,68 @@
+use crate::core;
+use crate::core::{Observer, Subscriber};
+use crate::subscriber::UnsubscribeMemorizingSubscriber;
 
+pub struct IntoIterObservable<IntoIter> {
+    iterable: IntoIter,
+}
+
+impl<IntoIter> IntoIterObservable<IntoIter>
+where
+    IntoIter: IntoIterator,
+{
+    fn new(iterable: IntoIter) -> Self {
+        Self { iterable }
+    }
+}
+
+impl<IntoIter> core::Observable for IntoIterObservable<IntoIter>
+where
+    IntoIter: IntoIterator,
+{
+    type Item = IntoIter::Item;
+    type Error = ();
+
+    fn subscribe<ObserverType>(self, observer: ObserverType)
+    where
+        ObserverType: core::Observer<Self::Item, Self::Error> + Send + Sync + 'static,
+    {
+        let mut subscriber = UnsubscribeMemorizingSubscriber::new(observer);
+        subscriber.on_subscribe(Box::new(subscriber.create_subscription()));
+        for v in self.iterable.into_iter() {
+            if !subscriber.is_unsubscribed() {
+                subscriber.on_next(v);
+            }
+        }
+        subscriber.on_completed();
+    }
+}
+
+impl<IntoIter> core::IntoObservable for IntoIter
+where
+    IntoIter: IntoIterator,
+{
+    type ObservableType = IntoIterObservable<IntoIter>;
+
+    fn into_observable(self) -> <Self as core::observable::IntoObservable>::ObservableType {
+        IntoIterObservable::new(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::observer;
+    use crate::prelude::*;
+    use std::sync::{Arc, Mutex};
+
+    #[test]
+    fn iterable_into_observable() {
+        let vec = vec![0, 1, 2, 3];
+        let sum = Arc::new(Mutex::new(0));
+        let sum_move = sum.clone();
+        vec.into_observable()
+            .subscribe(observer::from_next_fn(move |v| {
+                (*sum_move.lock().unwrap()) += v
+            }));
+        assert_eq!((*sum.lock().unwrap()), 6);
+    }
+}
