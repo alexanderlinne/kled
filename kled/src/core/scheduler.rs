8bfdd1d2d0c912f749ade68047f0c47ce88fb729
Alexander Linne <alexander.linne@outlook.com>
2020-10-20 23:28:59 +0200

    remove scheduler::Worker

--- kled/src/core/scheduler.rs
+++ kled/src/core/scheduler.rs
@@ -1,7 +1,6 @@
 use std::future::Future;
 #[chronobreak]
 use std::time;
-use crate::scheduler;
 
 pub trait Scheduler: Clone + Send + Sync + 'static {
     fn schedule<Fut>(&self, future: Fut)
@@ -11,13 +10,6 @@ pub trait Scheduler: Clone + Send + Sync + 'static {
     fn schedule_delayed<Fut>(&self, delay: time::Duration, future: Fut)
     where
         Fut: Future<Output = ()> + Send + 'static;
-
-    fn worker<State>(&self, state: State) -> scheduler::Worker<State>
-    where
-        State: Send + 'static,
-    {
-        scheduler::Worker::new(self.clone(), state)
-    }
 
     fn join(&self);
 }
--- kled/src/scheduler/mod.rs
+++ kled/src/scheduler/mod.rs
@@ -2,5 +2,4 @@ reexport_all! {
     mod delay_channel;
     mod new_thread;
     mod thread_pool;
-    mod worker;
 }
